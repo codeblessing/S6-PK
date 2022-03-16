06710e4e1b9c64fbe8ba704f5c543e63273e7104
Jakub Kwiatkowski <jakub.m.kwiatkowski@gmail.com>
2022-03-16 13:13:31 +0100

    First draft

--- aes.py
+++ aes.py
@@ -2,4 +2,23 @@ from Crypto.Cipher import AES
 
 key = b"128-bit long key"
 
-cipher = AES.new(key, AES.MODE_ECB)
+encryptors = {
+    "ECB": AES.new(key, AES.MODE_ECB),
+    "CBC": AES.new(key, AES.MODE_CBC),
+    "CTR": AES.new(key, AES.MODE_CTR),
+    "CFB": AES.new(key, AES.MODE_CFB),
+    "OFB": AES.new(key, AES.MODE_OFB)
+}
+
+files = [
+    "data/small.txt",
+    "data/medium.txt",
+    "data/large.txt"
+]
+
+with open("results.txt", "w") as results:
+    for file in files:
+        with open(file) as file:
+            pass
+
+# with open("data/")
